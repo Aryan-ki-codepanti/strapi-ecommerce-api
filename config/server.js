89dc77143ed50c7955d25eca8d07d6c92823126c
Aryan Sethi <aryan.rdps@gmail.com>
2021-11-30 10:23:08 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4949ac05e53fd774d88270eb2f21faeb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17feb73f-a660-454e-b7c2-0a5717655b3b'
+};
